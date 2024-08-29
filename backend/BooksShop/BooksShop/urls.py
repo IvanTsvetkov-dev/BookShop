1961d748c55c6edcef5ee9e1dee086cabcd8da5e
IvanTsvetkov-dev <vvvanusha62@mail.ru>
2024-08-30 01:32:39 +0300

    changed names

--- backend/BooksShop/BooksShop/urls.py
+++ backend/BooksShop/BooksShop/urls.py
@@ -27,12 +27,9 @@ from rest_framework.permissions import *
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Books API",
       default_version='v1',
       description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
    ),
    public=True,
    permission_classes=[AllowAny],
