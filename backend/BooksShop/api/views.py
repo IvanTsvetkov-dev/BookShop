33c4b5caf96cfab3a7b526cf5358117aa24653a4
IvanTsvetkov-dev <vvvanusha62@mail.ru>
2024-09-08 22:31:06 +0300

    Added patch and delete for / api/v1/basket/2/ uri

--- backend/BooksShop/api/views.py
+++ backend/BooksShop/api/views.py
@@ -21,16 +21,34 @@ class BookViewSet(mixins.ListModelMixin,
 class BasketViewSet(mixins.RetrieveModelMixin,
                     mixins.CreateModelMixin,
                     mixins.ListModelMixin,
+                    mixins.DestroyModelMixin,
+                    mixins.UpdateModelMixin,
                     viewsets.GenericViewSet):
     permission_classes = [IsAuthenticated]
     
     serializer_class = BasketSerializer
     
     def get_queryset(self):
-        print(self.request.user.id)
         return models.Basket.objects.filter(user_id=self.request.user.id)
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+    def destroy(self, request, pk=None):
+        basket_item = self.get_queryset().filter(book_id=pk).first()
+        if not basket_item:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        self.perform_destroy(basket_item)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    def update(self, request, pk, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_queryset().filter(book_id=pk).first()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
     
 class RandomQuote(APIView):
     def get(self, request):
